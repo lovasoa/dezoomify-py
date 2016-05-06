lovasoa/dezoomify-py

commit fcccb3a595db3e17062d9edc407347ee68498b47

In batch mode, on download errors, log it
In batch mode, when at least one download failed, print their name to a file.
Fixes #4

dezoomify.py
@@ -209,6 +209,7 @@ class ImageUntiler():
             self.process_image(self.image_urls[0], self.out_names[0])
             self.log.info("Dezoomifed image created and saved to {}.".format(self.out_names[0]))
         else:
+            failed_downloads = []
             zoom_level = self.zoom_level
             for i, image_url in enumerate(self.image_urls):
                 self.zoom_level = zoom_level # Reinitialize zoom level between each download
@@ -218,8 +219,17 @@ class ImageUntiler():
                     self.process_image(image_url, destination)
                     self.log.info("Dezoomifed image created and saved to {}.".format(destination))
                 except Exception as e:
+                    # The file couldn't be downloaded. Add it to the list of failed downloads
+                    failed_downloads.append((image_url, destination))
                     if not isinstance(e, (FileNotFoundError, JpegtranException, ZoomLevelError)):
                         self.log.warning("Unknown exception occurred while processing image {}: {} ()".format(image_url, e.__class__.__name__, e))
+
+            if len(failed_downloads) > 0:
+                with open("failed-downloads.txt", 'w') as failed_downloads_file:
+                    for image_info in failed_downloads:
+                        failed_downloads_file.write("%s\t%s\n" % image_info)
+                    self.log.warning("Processing of some files have failed. Their URLs and names have been saved to '%s'."
+                                    % (failed_downloads_file.name))
 
     def process_image(self, image_url, destination):
         """Scrapes image info and calls the untiler."""

commit 92fc28b74d7efc1d0112ddbd489849bf84442a73

Batch mode: Resolve file names at the last moment
Allows to have correct file names in the error file introduced by
fcccb3a595db3e17062d9edc407347ee68498b47

dezoomify.py
@@ -216,7 +216,7 @@ class ImageUntiler():
                 destination = self.out_names[i]
                 self.log.info("[{}/{}] Processing image {}...".format(i + 1, len(self.image_urls), image_url))
                 try:
-                    self.process_image(image_url, destination)
+                    self.process_image(image_url, os.path.join(os.path.dirname(self.out), destination))
                     self.log.info("Dezoomifed image created and saved to {}.".format(destination))
                 except Exception as e:
                     # The file couldn't be downloaded. Add it to the list of failed downloads
@@ -503,7 +503,7 @@ class ImageUntiler():
                         m = re.search('\\.' + self.ext + '$', line[1])
                         if not m:
                             line[1] += '.' + self.ext
-                        self.out_names.append(os.path.join(os.path.dirname(self.out), line[1]))
+                        self.out_names.append(line[1])
                     else:
                         continue
 